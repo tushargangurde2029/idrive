edbeec18b48bd44710fbc687c129152bf9fd680c
Tushar <tushargangurde405@gmail.com>
2021-03-29 00:04:50 +0530

    database changed to sqlite3

--- idrive/settings.py
+++ idrive/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'idrive.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'drivedata',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
